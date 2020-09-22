906552443a75154819611c2b139c21c74a397ce9
tommygratschew <tommy.gratschew@myy.haaga-helia.fi>
2020-09-22 20:09:14 +0300

    Bootstrap lisäykset

--- src/main/java/hh/swd20/betterbookstore/web/BookController.java
+++ src/main/java/hh/swd20/betterbookstore/web/BookController.java
@@ -39,4 +39,11 @@ public class BookController {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
+	
+	@RequestMapping(value = "/update/{id}")
+	public String updateBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+		return "updatebook";
+	}
+	
 }
